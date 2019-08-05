14e37ed69a87edec20cf85914b9f265703d4d599
ismail kaşan <ismailkasan@piton.com.tr>
2019-08-05 17:19:24 +0300

    ilk commit

---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SingleClass tek = SingleClass.getInstance();
+
+            Console.WriteLine("\n Ilk Instance: {0}", tek.deger);
+
+            SingleClass tek2 = SingleClass.getInstance();
+            Console.WriteLine("\n Ikinci Instance: {0}", tek2.deger);
+        }
+    }
+}
---
+++ Singleton/SingleClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class SingleClass
+    {
+        private SingleClass()
+        {
+            Random r = new Random();
+            deger = r.Next();
+        }
+        private static SingleClass _instance;
+        private static SingleClass instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SingleClass();
+                }
+                return _instance;
+
+            }
+            set
+            {
+                _instance = value;
+            }
+
+        }
+        public int deger;
+        public static SingleClass getInstance()
+        {
+            return instance;
+        }
+    }
+}
